ccdfe20d3fbea819c33d55b8a2eabf03a4b8857f
clogerie <66565287+clogerie@users.noreply.github.com>
2022-06-11 16:54:49 -0400

    cleaned up tests

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -11,25 +11,25 @@ describe("Employee", () => {
     });
     describe("get name", () => {
         it("should return the employee's name", () => {
-            const employee = new Employee ("Tony Stark", 2, "ironman@avengers.com");
-            expect(employee.getName()).toEqual("Tony Stark");
+            const employee = new Employee("Steve Rogers", 1, "captainamerica@avengers.com");
+            expect(employee.getName()).toEqual("Steve Rogers");
         });
     });
     describe("get id", () => {
         it("should return the employees id", () => {
-            const employee = new Employee ("Natasha Romanoff", 3, "blackwidow@avengers.com");
-            expect(employee.getId()).toEqual(3);
+            const employee = new Employee("Steve Rogers", 1, "captainamerica@avengers.com");
+            expect(employee.getId()).toEqual(1);
         });
     });
     describe("get email", () => {
         it("should return the employee's email address", () => {
-            const employee = new Employee("Bruce Banner", 4, "hulk@avengers.com");
-            expect(employee.getEmail()).toEqual("hulk@avengers.com");
+            const employee = new Employee("Steve Rogers", 1, "captainamerica@avengers.com");
+            expect(employee.getEmail()).toEqual("captainamerica@avengers.com");
         });
     });
     describe("get role", () => {
         it("should return the employee's role", () => {
-            const employee = new Employee("Thor", 5, "thor@avengers.com");
+            const employee = new Employee("Steve Rogers", 1, "captainamerica@avengers.com");
         expect(employee.getRole()).toEqual("Employee");
         });
     });
