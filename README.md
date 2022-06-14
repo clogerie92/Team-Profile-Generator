fdda9ee1e5dc7801e97356a2a759262613ac5a14
clogerie <66565287+clogerie@users.noreply.github.com>
2022-06-14 17:58:09 -0400

    added icons and changed color of navbar and card header, added content to README file

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # Team-Profile-Generator
+
+## Description
+
+This is a Node.js command-line application that takes in information about employees on a software engineering team, then generates an HTML webpage that displays summaries for each person.
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Mock-Up
+
+The following image shows a mock-up of the generated HTML’s appearance and functionality:
+
+![HTML webpage titled "Team Profile Generator” features five boxes listing employee names, titles, and other key info.]()
+
+
+
+## Installation 
+The user should clone the repository from GitHub and install Node. This application also requires a file system, inquirer module and uses Jest for testing. 
+
+## Usage 
+Use inquirer from your command line to answer questions about your project.
+View walk through video here - [Screencastify]()<br>
+<img src="./images/">
+
+## License 
+This project is license under MIT
+
+## Contributing 
+Contributors should read the installation section. 
+
+## Tests
+Run `npm test` to run Jest for tests. 
+
+## Questions
+Please contact me at carl.logerie92@gmail.com with any questions.
--- src/generateCards.js
+++ src/generateCards.js
@@ -4,10 +4,12 @@ const createManagerCard = function(manager) {
     return `
     <div class="col-4 mt-4">
         <div class="card">
-            <h5 class="card-header">${manager.name}</h5>
+            <div class="card-header bg-secondary>
+                <h5>${manager.name}</h5>
+            </div>
             <div class="card-body">
-                <h5 class="card-title">Manager</h5>
-                <p class="email">Email:<a href="email at:${manager.email}">${manager.email}</a></p>
+                <h5 class="card-title">Manager<i class="bi bi-clipboard2"></i></h5>
+                <p class="email">Email:<a href="${manager.email}">${manager.email}</a></p>
                 <p class="office-number">Office Number: ${manager.officeNumber}</p>
             </div>
         </div>
@@ -19,11 +21,13 @@ const createEngineerCard = function(engineer) {
     return `
     <div class="col-4 mt-4">
         <div class="card">
-            <h5 class="card-header">${engineer.name}</h5>
+            <div class="card-header bg-secondary">
+                <h5>${engineer.name}</h5>
+            </div>
             <div class="card-body">
-                <h5 class="card-title">Engineer</h5>
+                <h5 class="card-title">Engineer<i class="bi bi-pc-display-horizontal"></i></h5>
                 <p class="email">Email:<a href="email at:${engineer.email}">${engineer.email}</a></p>
-                <p class="github">GitHub: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+                <p class="github">GitHub: <a href="https://github.com/${engineer.gitHub}">${engineer.gitHub}</a></p>
             </div>
         </div>
     </div>
@@ -34,9 +38,11 @@ const createInternCard = function(intern) {
     return `
     <div class="col-4 mt-4">
         <div class="card">
-            <h5 class="card-header">${intern.name}</h5>
+            <div class="card-header bg-secondary">
+                <h5>${intern.name}</h5>
+            </div>
             <div class="card-body">
-                <h5 class="card-title">Intern</h5>
+                <h5 class="card-title">Intern<i class="bi bi-person-badge"></i></h5>
                 <p class="email">Email:<a href="email at:${intern.email}">${intern.email}</a></p>
                 <p class="school">School: ${intern.school}</p>
             </div>
@@ -65,6 +71,28 @@ createHtml = (employeeData) => {
         }
     }
 
+    // cardsArray.forEach(card => {
+    //     const employee = employeeData[i];
+    //     const role = employee.getRole();
+        
+    //     // switch(role) {
+    //     //     case "Manager":
+    //     //         const managerCard = createManagerCard(employee);
+    //     //         cardsArray.push(managerCard);
+    //     //         break;
+    //     //     case "Engineer":
+    //     //         const engineerCard = createEngineerCard(employee);
+    //     //         cardsArray.push(engineerCard);
+    //     //         break;
+    //     //     case "Intern":
+    //     //         const internCard = createInternCard(employee);
+    //     //         cardsArray.push(internCard);
+    //     //         break;
+    //     //     default:
+    //     //         console.log("Please select a role!");
+    //     // }
+    // });
+
     const teamCards = cardsArray.join("");
     const teamPage = teamWebPage(teamCards);
     return teamPage;
@@ -78,15 +106,19 @@ const teamWebPage = function(employeeCards) {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="./dist/style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Abel&family=Macondo&family=Montserrat:wght@700&display=swap" rel="stylesheet">
         <title>Team Profile Generator</title>
     </head>
     <body>
         <header>
-            <nav class="navbar navbar-expand-lg bg-light">
+            <nav class="navbar navbar-expand-lg bg-secondary">
                 <div class="container-md">
-                    <span>Team Profile Generator</span>
+                    <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile Generator</span>
                 </div>
             </nav>
         </header>
