33b4c1473b6a798314e8b59eea3a2ee7ab3615f9
clogerie <66565287+clogerie@users.noreply.github.com>
2022-06-13 21:27:57 -0400

    consolidated prompts for engineer and intern into oneaddEmployee prompt

--- index.js
+++ index.js
@@ -36,4 +36,64 @@ const addManager = () => {
     });
 }
 
-addManager();
+const addEmployee = () => {
+    return inquirer.prompt([
+        {
+            type: "list",
+            message: "Please select your employees role.",
+            choices: ["Engineer", "Intern"],
+            name: "role"
+        },
+        {
+            type: "input",
+            message: "Please enter the emplpoyee's name.",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Please enter the emplpoyee's id.",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Please enter the emplpoyee's email.",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Please enter the emplpoyee's school.",
+            name: "school",
+            when: (selected) => selected.role === "Intern"
+        },
+        {
+            type: "input",
+            message: "Please enter the emplpoyee's GitHub username.",
+            name: "github",
+            when: (selected) => selected.role === "Engineer"
+        },
+        {
+            type: "confirm",
+            message: "Would you like to add another employee?",
+            name: "confirmNewEmployee"
+        },
+    ]).then(employeeInfo => {
+        const {name, id, email, role, school, github, confirmNewEmployee} = employeeInfo;
+        let newEmployee;
+
+        if(role === "Engineer") {
+            newEmployee = new Engineer(name, id, email, github);
+            console.log("Added: ", newEmployee);
+        } else if (role === "Intern") {
+            newEmployee = new Inter(name, id, email, school);
+            console.log("Added: ", newEmployee)
+        }
+        team.push(newEmployee);
+
+        if(confirmNewEmployee) {
+            return addEmployee(team);
+        } else {
+            return team;
+        }
+    });
+};
+
