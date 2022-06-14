c5f6d47655cfe1c69d96ffa41b491a2476b5e423
clogerie <66565287+clogerie@users.noreply.github.com>
2022-06-13 20:50:37 -0400

    refactoring index.js file, creating new prompt for manager and employees

--- index.js
+++ index.js
@@ -2,110 +2,38 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
 const Manager = require("./lib/Manager");
-const employees = [];
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const team = [];
 
-managerQuestions().then((response) => {
-    console.log(response);
-    const htmlFile = "dist/index.html";
-    fs.writeFile(path.resolve(__dirname, htmlFile), "test", (error) => {
-        if (error) {
-            console.log(error)
-        } else {
-            console.log("Worked!")
-        }
-    });
-});
-
-function engineerQuestions() {
+const addManager = () => {
     return inquirer.prompt([
         {
             type: "input",
-            message: "Please enter the engineer's name",
+            message: "Please enter the manager's name.",
             name: "name"
         },
         {
             type: "input",
-            message: "Please enter the engineer's email address",
-            name: "email"
-        },
-        {
-            type: "input",
-            message: "Please enter the engineer's github username",
-            name: "github"
-        },
-        {
-            type: "input",
-            message: "Please enter the engineer's id",
-            name: "id"
-        }
-    ]).then((response) => {
-        console.log(response);
-        return new Engineer(response.name, response.email, response.id, response.github);
-    });
-}
-
-function internQuestions() {
-    return inquirer.prompt([
-        {
-            type: "input",
-            message: "Please enter the intern's name",
-            name: "name"
-        },
-        {
-            type: "input",
-            message: "Please enter the intern's email address",
-            name: "email"
-        },
-        {
-            type: "input",
-            message: "Please enter the intern's school",
-            name: "school"
-        },
-        {
-            type: "input",
-            message: "Please enter the intern's id",
-            name: "id"
-        }
-    ]).then((response) => {
-        console.log(response);
-        return new Intern(response.name, response.email, response.id, response.school);
-    });
-}
-
-function managerQuestions() {
-    return inquirer.prompt([
-        {
-            type: "input",
-            message: "Please enter your name",
-            name: "name"
-        },
-        {
-            type: "input",
-            message: "Please enter your id",
+            message: "Please enter the manager's id.",
             name: "id"
         },
         {
             type: "input",
-            message: "Please enter your email",
+            message: "Please enter the manager's email.",
             name: "email"
         },
         {
             type: "input",
-            message: "Please enter your office number",
+            message: "Please enter the manager's office number.",
             name: "officeNumber"
         },
-        {
-            type: "list",
-            message: "Would you like to add an employee, engineer, or intern?",
-            choices: ["employee", "engineer", "intern"],
-            name: "team"
-        }
-    ]).then((response) => {
-        console.log(response);
-        return new Manager(response.name, response.email, response.id, response.officeNumber);
+    ]).then(managerInfo => {
+        const {name, id, email, officeNumber} = managerInfo;
+        const manager = new Manager(name, id, email, officeNumber);
+        console.log("Added: ", manager);
+        team.push(manager);
     });
 }
 
-
-// engineerQuestions();
-// internQuestions();
+addManager();
